a06137781fee558329e1a76c9d9e7bff3b00223d
Tito-74 <kipkirui133@gmail.com>
2022-03-15 09:54:32 +0300

    creating jwt token

--- routes/auth.go
+++ routes/auth.go
@@ -1,18 +1,26 @@
 package routes
 
 import (
-	"JWT-GoFiber/Database"
+	database "JWT-GoFiber/Database"
 	"JWT-GoFiber/models"
+
+	// "strconv"
+	"fmt"
+	"time"
+
+	// "logger"
+
+	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
-	"github.com/dgrijalva/jwt-go/v4"
-	"strconv"
-	"time"
+	
 )
 
-const SecretKey = "secretkey"
 
-//  Time := time.Date(2022, 03, 9, 7, 0, 0, 0, time.UTC)
+
+const SecretKey = "secret"
+
+// type typetime time.Time
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -20,17 +28,17 @@ func Register(c *fiber.Ctx) error {
 	if err := c.BodyParser(&data); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
-	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14) 
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 	user := models.User{
-		Name:			data["name"],
-		Email : 	data["email"],
+		Name:     data["name"],
+		Email:    data["email"],
 		Password: password,
 	}
 	database.Database.Db.Create(&user)
 	return c.JSON(user)
 }
 
-func Login(c *fiber.Ctx)error{
+func Login(c *fiber.Ctx) error {
 	var data map[string]string
 
 	if err := c.BodyParser(&data); err != nil {
@@ -43,37 +51,73 @@ func Login(c *fiber.Ctx)error{
 
 	if user.Id == 0 {
 		c.Status(404)
+		fmt.Printf("Wrong user...\n")
 		return c.JSON(fiber.Map{
-			"Message":"User not found",
+			"Message": "Wrong Credentials",
 		})
 	}
-	
-	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil{
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
 		c.Status(400)
+		fmt.Printf("Wrong password...\n")
 		return c.JSON(fiber.Map{
-			"Message":"Wrong Password",
+			"Message": "Wrong Credentials",
 		})
-		// .JSON(err.Error())
+		
 	}
 
 
-	claims := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.StandardClaims{
-		Issuer:    strconv.Itoa(int(user.Id)),
-		ExpiresAt: time.Time(),
-		// Add(time.Hour * 24).Unix(), Bug to handle
-	})
-
-	token, err := claims.SignedString([]byte(SecretKey))
-
+	
+	token, ExpiresAt, err := CreateJwtToken(&user)
 	if err != nil {
-		c.Status(500)
-		c.JSON(fiber.Map{
-			"Message":"Could not log in",
-		})
+		return err
+
+		
 	}
 
-	return c.JSON(token)
+	// c.Cookie(cookie)
 
-
+	return c.JSON(fiber.Map{"token": token, "exp": ExpiresAt})
+	
+	
 
 }
+
+func CreateJwtToken(user *models.User) (string, int64, error) {
+	ExpiresAt := time.Now().Add(time.Hour * 24).Unix()
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["user_id"] = user.Id
+
+	claims["expires"] = ExpiresAt
+
+	t, err := token.SignedString([]byte(SecretKey))
+
+	if err != nil {
+
+		return "", 0, err
+	}
+
+	return t, ExpiresAt, nil
+
+}
+
+// cookie = fiber.Cookie{
+// 	Name:"jwt",
+// 	Value: token, 
+// 	Expires: time.Now().Add(time.Hour * 24).Unix(),
+// 	HTTPOnly: true,
+// }
+// c.Cookie(cookie)
+
+
+
+
+	
+
+
+
+
